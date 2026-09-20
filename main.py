87bc8c398039e04e568d0e76d56e1a9d6bdb20a5
Dilip <dlipsuthar6367@gmail.com>
2026-09-20 16:23:33 +0530

    feature: difference function added and implemented in main.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from adding import add
+from difference import diff
+
 def main():
     print("Hello I am Dilip!")
+    x=10
+    y=12
+    print(add(x,y))
+    print(diff(x,y))
 
 
 if __name__ == "__main__":
